8b003f1865e2dd0e7fc3fc94f57de1df51a7abfc
sumit <mazumdarsumit3@gmail.com>
2024-05-02 18:03:17 +0530

    created login flow

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,6 +3,7 @@ import bcrypt from 'bcrypt';
 import User from '../models/user.model.js';
 import { fromError } from 'zod-validation-error';
 import { errorHandler } from '../utils/errorhandler.js';
+import jwt from 'jsonwebtoken';
 
 // zod schema for registration
 const regSchema = z.object({
@@ -35,6 +36,52 @@ export const registration = async (req, res, next) => {
       success: true,
       message: 'user created successfully',
     });
+  } catch (err) {
+    if (err instanceof z.ZodError) {
+      const validationError = fromError(err);
+      return next(errorHandler(400, validationError.toString()));
+    } else {
+      return next(err);
+    }
+  }
+};
+
+const loginSchema = z.object({
+  username: z.string().min(3).max(30),
+  password: z.string().min(6),
+});
+
+/**
+ * @LOGNIN
+ * @ROUTE @POST {{url}}/api/auth
+ * @access PUBLIC
+ */
+
+export const login = async (req, res, next) => {
+  try {
+    const { username, password } = loginSchema.parse(req.body);
+
+    // Find user by username
+    const validUser = await User.findOne({ username });
+    if (!validUser) {
+      return next(errorHandler(404, 'User not found'));
+    }
+    // Compare passwords
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, 'Wrong Credentials'));
+    }
+    // Generate JWT token
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    // Omit password field from response
+    const { password: pass, ...userData } = validUser._doc;
+
+    // Set JWT token in cookie and send user data in response
+    res
+      .cookie('access_token', token, { httpOnly: true })
+      .status(200)
+      .json({ userData, message: 'user logged in successfully' });
   } catch (err) {
     if (err instanceof z.ZodError) {
       const validationError = fromError(err);
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { registration } from '../controllers/auth.controller.js';
+import { login, registration } from '../controllers/auth.controller.js';
 
 const router = Router();
 
 router.post('/registration', registration);
+router.post('/login', login);
 
 export default router;
